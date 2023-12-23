11a36701635e51e0c027ca5e356d37a479e67e4b
Oran <oran@cyberpion.com>
2023-12-23 21:12:44 +0200

    done

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,3 @@
-from multi_tenant_application.api.models.findings import *
-from multi_tenant_application.api.models.resources import *
-from multi_tenant_application.api.models.tenants import *
+from api.models.findings import *
+from api.models.resources import *
+from api.models.tenants import *
